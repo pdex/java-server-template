8ef7e065bd21e2a5820e831ae042735716081a67
Chris Keating <christopherkeating@gmail.com>
2017-06-08 14:16:56 -0700

    Added structure to config, removed CONSTANTS

--- {{cookiecutter.app_name}}/src/main/java/{{cookiecutter.java_package_dir}}/XConfig.java
+++ {{cookiecutter.app_name}}/src/main/java/{{cookiecutter.java_package_dir}}/XConfig.java
@@ -11,45 +11,56 @@ public class XConfig {
   }
 
   public int readerIdleTimeout() {
-    return config.getInt("reader_idle_timeout");
+    return config.getInt("limits.reader_idle_timeout");
   }
 
   public int writerIdleTimeout() {
-    return config.getInt("writer_idle_timeout");
+    return config.getInt("limits.writer_idle_timeout");
   }
 
   public int requestIdleTimeout() {
-    return config.getInt("request_idle_timeout");
+    return config.getInt("limits.request_idle_timeout");
   }
 
-  public String workerNameFormat() {
-    return config.getString("worker_name_format");
+  public String bossNameFormat() {
+    return config.getString("boss.name_format");
   }
 
   public int bossThreads() {
-    return config.getInt("boss_threads");
+    return config.getInt("boss.threads");
+  }
+
+  public String workerNameFormat() {
+    return config.getString("worker.name_format");
   }
 
   public int workerThreads() {
-    return config.getInt("worker_threads");
+    return config.getInt("worker.threads");
   }
 
 
   public int maxConnections() {
-    return config.getInt("max_connections");
+    return config.getInt("limits.max_connections");
   }
 
   public float rateLimit() {
-    return (float) config.getInt("req_per_sec");
+    return (float) config.getInt("limits.req_per_sec");
   }
 
+  public int httpMessageSize() {
+    return config.getBytes("limits.http_message_size").intValue();
+  }
 
   public String cert() {
-    return config.getString("cert");
+    return config.getString("tls.cert");
   }
 
   public String key() {
-    return config.getString("key");
+    return config.getString("tls.key");
+  }
+
+  public String ip() {
+    return config.getString("server.ip");
   }
 
   public int port() {
